b4a36fb40f90050a82377bac51a3e89e96be2322
LeMinhSon <leminhsonphysics@gmail.com>
2018-08-06 22:35:49 +0700

    Add X-frame

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var xFrameOptions = require('x-frame-options')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +16,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(xFrameOptions(headerValue = 'ALLOWALL'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
