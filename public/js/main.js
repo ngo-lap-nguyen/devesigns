ab872e061ce26d5d2ac5f5b32e156808650e0150
Nguyen Ngo Lap <ngolap.nguyen@gmail.com>
2018-11-11 05:28:30 +0700

    Section menu change; responsive design

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,36 @@
-$("#home .container").css('height', 'calc(' + (window.innerHeight - $('.site-header .header-container')[0].scrollHeight) + 'px')
+var headerActive = false;
 
+// ---
+var mqLong = window.matchMedia("(min-width: 768px) and (max-aspect-ratio: 10/13)");
+
+if (mqLong.matches) {
+    $("#home .container").css('height', 'calc(' + ($(window).innerHeight() - 80) / 2 + 'px')
+}
+else {
+    $("#home .container").css('height', 'calc(' + ($(window).innerHeight() - 80) + 'px')
+}
+
+window.addEventListener('resize', () => {
+    if (mqLong.matches) {
+        $("#home .container").css('height', 'calc(' + ($(window).innerHeight() - 80) / 2 + 'px')
+    }
+    else {
+        $("#home .container").css('height', 'calc(' + ($(window).innerHeight() - 80) + 'px')
+    }
+})
+
+// ---
 window.onbeforeunload = () => {
     window.scrollTo(0, 0);
 }
 
 $(document).ready(() => {
+    // $(function() {  
+    //     $("body").niceScroll({
+    //         horizrailenabled: false
+    //     });
+    // });
+
     $('#scroll-prompt').on('click', () => {
         $('html, body').animate({
             scrollTop: $("#services").offset().top,
@@ -12,11 +38,17 @@ $(document).ready(() => {
         }, 1000);
     })
 
-    window.addEventListener('scroll', (e) => {
+    $(window).scroll((e) => {
+        if ($(window).scrollLeft() != 0) {
+            $(window).scrollLeft(0);
+        }
+
         if ($(window).scrollTop() > 0) {
             $('.site-header').addClass('active');
+            headerActive = true;
         } else {
             $('.site-header').removeClass('active');
+            headerActive = false;
         }
         
         setTimeout(() => {
@@ -27,6 +59,15 @@ $(document).ready(() => {
             }
         }, 300);
     });
+
+    // Mobile menu
+    $(".site-header .menu-mobile-button").on('click', () => {
+        if (!headerActive) {
+            $('.site-header').toggleClass('active');
+        }
+        $('.site-header .menu-mobile').toggleClass('active');
+        
+    })
 
     // Project roulette
     $('#project-detail-carousel').slick({
@@ -60,9 +101,35 @@ $(document).ready(() => {
     $('#project-detail .exit-button').click(() => {
         $('#project-detail').removeClass('active');
     })
-});
 
-// Internal
+    $('.main-section').attr('data-appear-top-offset', '-' + window.innerHeight / 2 + '');
+
+    // On appear
+    $('#home').appear();
+    $('#services').appear();
+    $('#work').appear();
+    $('#contact').appear();
+
+    $('#home').on('appear', () => {
+        $('.site-header .menu-option').removeClass('active');
+        $('.site-header .menu-option.home').addClass('active');
+    })
+
+    $('#services').on('appear', () => {
+        $('.site-header .menu-option').removeClass('active');
+        $('.site-header .menu-option.services').addClass('active');
+    })
+
+    $('#work').on('appear', () => {
+        $('.site-header .menu-option').removeClass('active');
+        $('.site-header .menu-option.work').addClass('active');
+    })
+
+    $('#contact').on('appear', () => {
+        $('.site-header .menu-option').removeClass('active');
+        $('.site-header .menu-option.contact').addClass('active');
+    })
+});
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
